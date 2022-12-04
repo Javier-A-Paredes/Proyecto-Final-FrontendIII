8117f781a3a9b916f0363c4c94985ef520bee0aa
javier-a-paredes <javi4202@gmail.com>
2022-12-04 18:26:54 -0300

    subiendo form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
-
+import Form from "./Components/Form";
 
 function App() {
   return (
       <div className="App">
           <Navbar/>
+          <Form />
           <Footer/>
       </div>
   );
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,15 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { TextField, Button } from '@mui/material';
+import * as yup from 'yup';
+import { useFormik } from 'formik';
 
-
-const Form = () => {
+const Formulario = () => {
   //Aqui deberan implementar el form completo con sus validaciones
-
+  
+  
+  //validacion con yup
+  let schema = yup.object().shape({
+    nombre: yup.string().required("Debe ingresar un nombre").min(5,"El nombre completo debe tener al menos 5 letras"),
+    email: yup.string().required("Debe ingresar un email").email("Ingrese un email valido")
+  })
+  
+  const formik = useFormik({
+    initialValues: {
+      nombre: '',
+      email: '',      
+    },
+    validationSchema: schema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  
   return (
     <div>
-      <form>
+      <form onSubmit={formik.handleSubmit}>
+        <TextField           
+          sx={{margin:"5px"}}            
+          id="outlined-required"
+          label="Full Name"
+          name="nombre"
+          defaultValue= {formik.values.nombre}
+          onChange={formik.handleChange}          
+          error={formik.touched.nombre && Boolean(formik.errors.nombre)}
+          helperText={formik.touched.nombre && formik.errors.nombre}
+        />      
+        <TextField
+          sx={{margin:"5px"}}            
+          id="outlined-required"
+          name="email"
+          label="Email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+        />         
+        <Button color="primary" variant="contained" fullWidth type="submit">
+          Send
+        </Button>
       </form>
     </div>
   );
+  
 };
 
-export default Form;
+export default Formulario;
