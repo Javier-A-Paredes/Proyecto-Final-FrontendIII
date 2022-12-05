bfa034c6c29161ae5e0d25f7ecb38320cab8aadf
javier-a-paredes <javi4202@gmail.com>
2022-12-05 20:29:27 -0300

    comienzo card

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 import Form from "./Components/Form";
+import Card from "./Components/Card";
 
 function App() {
   return (
       <div className="App">
           <Navbar/>
           <Form />
+          <Card />
           <Footer/>
       </div>
   );
