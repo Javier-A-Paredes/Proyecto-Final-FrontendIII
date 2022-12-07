19f5170d2747829cb194269790871cc970d1b06d
javier-a-paredes <javi4202@gmail.com>
2022-12-07 19:04:15 -0300

    el verdadero card-grid

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 import Form from "./Components/Form";
 import Card from "./Components/Card";
-
+import Home from "./Routes/Home";
 function App() {
   return (
       <div className="App">
           <Navbar/>
           <Form />
-          <Card />
+          {/* <Card /> */}
+          <Home />
           <Footer/>
       </div>
   );
