82907285648b10f7bcc1f8e97cd5f56f584ec0b6
javier-a-paredes <javi4202@gmail.com>
2022-12-07 22:20:42 -0300

    ?

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -15,37 +15,39 @@ const Navbar = () => {
 
   const navigate = useNavigate();
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{padding: "10px 50px"}} >
-        <Toolbar>
-          <img src="./DH.ico" alt="DH icono" />
-          
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              <Link to='/'>
-                Odonto
-              </Link>
-            </Typography>
-          
-          
-          <Button onClick={()=> navigate("/")} color="inherit">Home</Button>
-          <Button onClick={()=> navigate("/contact")} color="inherit">Contact</Button>
-          <Button onClick={()=> navigate("/favs")} color="inherit">Favs</Button>
-          
-          <IconButton
-            onClick={()=> dispatch({type: "theme"})}
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }
-          }
-          >
-            {state.prefersDark?<DarkModeIcon/>:<LightModeIcon/>}
-          </IconButton>
-          
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <header>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" sx={{padding: "10px 50px"}} >
+          <Toolbar>
+            <img src="./DH.ico" alt="DH icono" />
+            
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                <Link to='/'>
+                  Odonto
+                </Link>
+              </Typography>
+            
+            
+            <Button onClick={()=> navigate("/")} color="inherit">Home</Button>
+            <Button onClick={()=> navigate("/contact")} color="inherit">Contact</Button>
+            <Button onClick={()=> navigate("/favs")} color="inherit">Favs</Button>
+            
+            <IconButton
+              onClick={()=> dispatch({type: "theme"})}
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }
+            }
+            >
+              {state.prefersDark?<DarkModeIcon/>:<LightModeIcon/>}
+            </IconButton>
+            
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </header>
   )
 }
 
