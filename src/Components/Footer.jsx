b3036c93d5dc144a845816b7568a7b4fc80860a0
IvanTarjan <ivantarjan2003@gmail.com>
2022-12-09 23:49:27 -0300

    Fin fin final real

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Box } from '@mui/system'
 import {Facebook, Instagram, Twitter, WhatsApp} from '@mui/icons-material';
 import { Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 
 
@@ -18,10 +19,19 @@ const Footer = () => {
       <Box display={"flex"} justifyContent={"space-between"} alignItems={"center"} bgcolor="primary.main" width={"100%"} height={"80px"} padding={"20px 40px"} overflow={"hidden"}>
         <img className='footer-logo' src="./images/DH.png" alt='DH-logo' />
         <Box display={"flex"} justifyContent={"space-between"} alignItems={"center"} width={"300px"} height={"50px"}>
-          <Facebook fontSize='large'/>
+          <a href="https://facebook.com/login/device-based/regular/login/" target="_blank">
+            <Facebook fontSize='large'/>
+          </a>
+          <a href='https://www.instagram.com/accounts/login/' target={"_blank"}>
           <Instagram fontSize='large'/>
-          <Twitter fontSize='large'/>
-          <WhatsApp fontSize='large'/>
+          </a>
+          <a href='https://twitter.com' target={"_blank"}>
+            <Twitter fontSize='large'/>
+          </a>
+          <a href='https://web.whatsapp.com' target={"_blank"}>
+            <WhatsApp fontSize='large'/>
+          </a>
+          
         </Box>
       </Box>
     </footer>
